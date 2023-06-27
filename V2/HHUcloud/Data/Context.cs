d23edaa544458cab07f8427166531c21961ee0f2
SaoJiaFei <848418127@qq.com>
2023-06-27 08:50:00 +0800

    add dbContext

---
+++ V2/HHUcloud/Data/Context.cs
@@ -0,0 +1,25 @@
+﻿using HHUcloud.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace HHUcloud.Data;
+
+public class Context : DbContext
+{
+
+    public DbSet<User>? User { get; set; }
+    public DbSet<Song>? Song { get; set; }
+    public DbSet<Artist>? Artist { get; set; }
+    public DbSet<ArtistHasSong>? ArtistHasSong { get; set; }
+    public DbSet<Album>? Album { get; set; }
+    public DbSet<AlbumHasSong>? AlbumHasSong { get; set; }
+    public DbSet<Playlist>? Playlist { get; set; }
+    public DbSet<PlayRecord>? PlayRecord { get; set; }
+    public DbSet<PlaylistHasSong>? PlaylistHasSong { get; set; }
+    public Context(DbContextOptions<Context> options)
+        : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
--- V2/HHUcloud/Program.cs
+++ V2/HHUcloud/Program.cs
@@ -1,4 +1,7 @@
 
+using HHUcloud.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace HHUcloud;
 
 public class Program
@@ -6,6 +9,10 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<Context>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("Context") ??
+                throw new InvalidOperationException("Connection string 'UserContext' not found.")));
 
         // Add services to the container.
 
@@ -22,6 +29,17 @@ public class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+
+            var context = services.GetRequiredService<Context>();
+            context.Database.EnsureCreated();
+            // DbInitializer.Initialize(context);
+        }
+
+        app.UseHttpsRedirection();
 
         app.UseAuthorization();
 
