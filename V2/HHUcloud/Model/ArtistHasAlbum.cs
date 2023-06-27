f690d81c8250de8df92eb533c5aa08b21277474b
SaoJiaFei <848418127@qq.com>
2023-06-27 14:03:56 +0800

    add new data model

---
+++ V2/HHUcloud/Model/ArtistHasAlbum.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HHUcloud.Model;
+
+[Table("ARTIST_HAS_ALBUM"), Comment("歌手的专辑列表")]
+[PrimaryKey(nameof(ArtistId), nameof(AlbumId))]
+public class ArtistHasAlbum
+{
+    [Comment("歌手ID"), ForeignKey(nameof(Artist.ArtistId))]
+    public long ArtistId { get; set; }
+
+    [Comment("专辑ID"), ForeignKey(nameof(Album.AlbumId))]
+    public long AlbumId { get; set; }
+}
